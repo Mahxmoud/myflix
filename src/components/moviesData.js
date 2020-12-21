ca26b8b320e3f0ad62824dc343b751050aa6082a
mahxmoud <mahmoud.jomaa@yahoo.com>
2020-12-21 12:48:15 +0200

    seprate file for movies data

---
+++ src/components/moviesData.js
@@ -0,0 +1,59 @@
+const moviesData = [
+    {
+        title: "Avengers: Endgame",
+        description:
+            "After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again to reinvigorate their trounced allies and restore balance.",
+        posterUrl:
+            "https://thenewsfetcher.com/wp-content/uploads/2020/02/817597-avengers-endgame-1024x576.jpg",
+        rate: 3,
+    },
+    {
+        title: "The Shawshank Redemption",
+        description:
+            "Andy Dufresne, a successful banker, is arrested for the murders of his wife and her lover, and is sentenced to life imprisonment at the Shawshank prison. He becomes the most unconventional prisoner",
+        posterUrl:
+            "https://traditiononline.org/wp-content/uploads/2019/11/13-Best-Shawshank.jpg",
+        rate: 5,
+    },
+    {
+        title: "In Time",
+        description:
+            "In a future where people stop ageing after 25 and need to buy time to live, the rich become immortal while others cease to exist. So, Will is on the run with a hostage in a desperate bid to survive.",
+        posterUrl:
+            "https://tirlapaula.files.wordpress.com/2014/04/in-time-2011-1024x768-761788.jpeg",
+        rate: 2,
+    },
+    {
+        title: "Pulp Fiction",
+        description:
+            "In the realm of underworld, a series of incidents intertwines the lives of two Los Angeles mobsters, a gangster's wife, a boxer and two small-time criminals.",
+        posterUrl:
+            "https://townsquare.media/site/295/files/2019/10/miramax.jpg?w=980&q=75",
+        rate: 4,
+    },
+    {
+        title: "Titanic",
+        description:
+            "Seventeen-year-old Rose hails from an aristocratic family and is set to be married. When she boards the Titanic, she meets Jack Dawson, an artist, and falls in love with him.",
+        posterUrl:
+            "https://scontent.ftun1-2.fna.fbcdn.net/v/t1.0-9/117357203_3544561898896579_7992014078248731844_n.jpg?_nc_cat=101&ccb=2&_nc_sid=e3f864&_nc_ohc=hiK0pNaObHoAX_RYe4b&_nc_ht=scontent.ftun1-2.fna&oh=7c83d2b14156da56d333bee80d1782ed&oe=5FF3E5FB",
+        rate: 5,
+    },
+    {
+        title: "Godzilla: King of the Monsters",
+        description:
+            "The members of Monarch, an crypto-zoological organisation, must rely on the Godzilla and Mothra to defeat King Ghidorah and Rodan, after the former awakens other dormant Titans to destroy the world.",
+        posterUrl:
+            "https://i.dailymail.co.uk/1s/2019/05/26/14/13976618-0-image-a-44_1558878671094.jpg",
+        rate: 3,
+    },
+    {
+        title: "Charlie and the Chocolate Factory",
+        description:
+            "Charlie, a young boy from an impoverished family, and four other kids win a tour of an amazing chocolate factory run by an imaginative chocolatier, Willy Wonka, and his staff of Oompa-Loompas.",
+        posterUrl:
+            "https://1.bp.blogspot.com/__U6Of0VhREw/TC3GImGPVLI/AAAAAAAAABM/p63OnE2p9Pk/w1200-h630-p-k-no-nu/charlie_chocolate_factory.jpg",
+        rate: 4,
+    }
+]
+export default moviesData
